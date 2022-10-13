9e3232b84bd0c4dc45115b5ce874106e7e19059c
ensn <ensn>
2022-10-13 13:57:36 +0800

    refactor: cookie demo

--- http/cookie/main.go
+++ http/cookie/main.go
@@ -3,28 +3,27 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/readcookie", ReadCookie)
-	http.HandleFunc("/writecookie", WriteCookie)
-	http.HandleFunc("/deletecookie", DeleteCookie)
-	http.ListenAndServe(":9090", nil)
+	http.HandleFunc("/login", MockLoginHandler)
+	http.HandleFunc("/user", MockGetUserInfoHandler)
+	http.ListenAndServe(":18080", nil)
 }
 
-func WriteCookie(w http.ResponseWriter, r *http.Request) {
-	// 创建新的本地cookie
+func MockLoginHandler(w http.ResponseWriter, r *http.Request) {
+	// 创建新的本地cookie，正常逻辑应该是：生成 - 本地存储 - 写入cookie，此处demo
 	cookie := http.Cookie{Name: "localCookie", Value: "GoLang", Path: "/", MaxAge: 86400}
 	http.SetCookie(w, &cookie)
 	w.Write([]byte("设置cookie成功"))
 }
 
-func ReadCookie(w http.ResponseWriter, r *http.Request) {
+func MockGetUserInfoHandler(w http.ResponseWriter, r *http.Request) {
 	// 读取cookie
 	cookie, err := r.Cookie("localCookie")
 	if err == nil {
 		cookieValue := cookie.Value
 		// 将数据写入http连接中
-		w.Write([]byte("cookie的值为：" + cookieValue))
+		w.Write([]byte("cookie的值为:" + cookieValue))
 	} else {
-		w.Write([]byte("读取cookie出错：" + err.Error()))
+		w.Write([]byte("读取cookie出错:" + err.Error()))
 	}
 }
 
